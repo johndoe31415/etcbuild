56a0c25c352c2494c69f7e55308aea9b0f04c45e
Johannes Bauer <joe@johannes-bauer.com>
2020-09-17 20:58:36 +0200

    Can now successfully build binutils
    Borrowed all the configuration from other scripts, worked perfectly on
    first run.

--- BuilderFactory.py
+++ BuilderFactory.py
@@ -19,15 +19,61 @@
 #
 #	Johannes Bauer <JohannesBauer@gmx.de>
 
+import os
+import shutil
+import subprocess
+from WorkDir import WorkDir
+
+class NoBuilderPresentException(Exception): pass
+
 class GenericBuilder():
 	def __init__(self, factory, pkg):
 		self._factory = factory
 		self._pkg = pkg
+		self._builddir = "/tmp/etcbuild/%s" % (os.urandom(8).hex())
+		os.makedirs(self._builddir)
+
+	@property
+	def builddir(self):
+		return self._builddir
+
+	@property
+	def extractdir(self):
+		return self.builddir + "/" + self._pkg["pkgname"] + "-" + self._pkg["version"]
+
+	def env(self, key):
+		return self._factory.env[key]
+
+	def unpack(self):
+		source = self._pkg["local_filename"]
+		if source.endswith(".tar.gz"):
+			with WorkDir(self.builddir):
+				subprocess.check_call([ "tar", "xzf", source ])
+		else:
+			raise NotImplementedError("Unpacking of file: %s" % (source))
+
+	def build(self):
+		raise NotImplementedError(__class__.__name__)
+
+	def install(self):
+		with WorkDir(self.extractdir):
+			subprocess.check_call([ "make", "install" ], env = self._factory.subprocess_env)
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, *args):
+		if self._factory.automatic_cleanup:
+			shutil.rmtree(self._builddir)
 
 class BuilderFactory():
-	def __init__(self, environment, subprocess_environment):
+	_REGISTRY = { }
+
+	def __init__(self, environment, subprocess_environment, automatic_cleanup = True, concurrent_processes = 1):
 		self._env = environment
 		self._subprocess_env = subprocess_environment
+		self._automatic_cleanup = automatic_cleanup
+		self._concurrent_processes = concurrent_processes
 
 	@property
 	def env(self):
@@ -37,5 +83,50 @@ class BuilderFactory():
 	def subprocess_env(self):
 		return self._subprocess_env
 
+	@property
+	def automatic_cleanup(self):
+		return self._automatic_cleanup
+
+	@property
+	def concurrent_processes(self):
+		return self._concurrent_processes
+
 	def get_builder(self, pkg):
-		return GenericBuilder(self, pkg)
+		if pkg["pkgname"] not in self._REGISTRY:
+			raise NoBuilderPresentException("No builder present for package: %s" % (pkg["pkgname"]))
+		builder_class = self._REGISTRY[pkg["pkgname"]]
+		return builder_class(self, pkg)
+
+	@classmethod
+	def register(cls, register_for):
+		def decorator(builder_class):
+			cls._REGISTRY[register_for] = builder_class
+			return builder_class
+		return decorator
+
+@BuilderFactory.register("binutils")
+class BinutilsBuilder(GenericBuilder):
+	def build(self):
+		with WorkDir(self.extractdir):
+			configure_cmd = [ "./configure" ]
+			configure_cmd += [ "--prefix=%s" % (self.env("prefix")) ]
+			if self.env("target") in [ "7tdmi", "cortex-m0", "cortex-m1", "cortex-m3", "cortex-m4" ]:
+				# ARM architectures
+				configure_cmd += [ "--target=arm-none-eabi" ]
+				if self.env("target") == "7tdmi":
+					configure_cmd += [ "--enable-interwork", "--enable-multilib" ]
+				else:
+					configure_cmd += [ "--enable-thumb" ]
+					configure_cmd += [ "--disable-interwork", "--disable-multilib" ]
+			elif self.env("target") == "avr":
+				configure_cmd += [ "--target=avr" ]
+			elif self.env("target") == "blackfin":
+				configure_cmd += [ "--target=bfin" ]
+				configure_cmd += [ "--without-gnu-ld" ]
+			else:
+				configure_cmd += [ "--target=%s" % (self.env("target")) ]
+			configure_cmd += [ "--disable-nls" ]
+			configure_cmd += [ "--disable-libssp" ]
+			subprocess.check_call(configure_cmd, env = self._factory.subprocess_env)
+
+			subprocess.check_call([ "make", "-j%d" % (self._factory.concurrent_processes) ], env = self._factory.subprocess_env)
--- README.md
+++ README.md
@@ -1,8 +1,88 @@
 # etcbuild
 Embedded toolchain builder is a set of scripts that allows building a
-customized gcc build for Cortex-M[0134] systems, including binutils, gcc,
+customized gcc build for ARM, AVR, Blackfin systems, including binutils, gcc,
 newlib and gdb. It is written in a flexible manner to allow for easy adaption
 of new packages or architectures.
+
+## Usage
+```
+usage: etcbuild [-h] [-e filename] [-p filename] [-d dirname]
+                [--concurrency processes] [--disable-verification]
+                [--no-cleanup] [--no-install] [-v]
+                target [target ...]
+
+Embedded toolchain build script.
+
+positional arguments:
+  target                Target(s) to build.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -e filename, --envfile filename
+                        Specifies an environment file; can be specified
+                        multiple times (order matters, later options supersede
+                        previous ones). Set of JSON files which define build
+                        environment parameters such as the target or
+                        installation directory.
+  -p filename, --packages filename
+                        Specifies package configuration file. Defaults to
+                        packages.json.
+  -d dirname, --package-dir dirname
+                        Specifies subdirectory in which to store downloaded
+                        package sources. Defaults to packages.
+  --concurrency processes
+                        Specifies number of concurrent processes to use.
+                        Defaults to 12.
+  --disable-verification
+                        Disables verification of hashes of downloaded files.
+                        Not recommended.
+  --no-cleanup          By default, temporary files during the build process
+                        are removed after the build is finished. This option
+                        keeps them around.
+  --no-install          Just build the packages, but do not install them.
+  -v, --verbose         Increases verbosity. Can be specified multiple times
+                        to increase.
+```
+
+The typical configuration is split into a local JSON file that contains your
+local build environment, such as:
+
+```json
+{
+	"gcc-prefix":	"~/bin/gcc"
+}
+```
+
+And a target-specific/configuration-specific configuration file, such as:
+
+```json
+{
+	"target":	"cortex-m3",
+	"prefix":	"{{gcc-prefix}}/cm3"
+}
+```
+
+Then, the build process can be easily invoked:
+
+```
+$ ./etcbuild -e env_joe.json -e env_cortex_m3.json binutils
+```
+
+or
+
+```
+$ ./etcbuild -e env_joe.json -e env_cortex_m3.json binutils gcc gdb
+```
+
+## Targets
+Currently recognized targets are:
+  * cortex-m0
+  * cortex-m1
+  * cortex-m3
+  * cortex-m4
+  * tdmi7
+  * avr
+  * blackfin
 
 ## License
 GNU GPL-3.
